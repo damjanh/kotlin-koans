95cd8f48640cb849cbe51576570cb1ad86995263
damjanh <damjan.hudobreznik@gmail.com>
2018-04-15 20:53:31 +0200

    Solved all tasks in *iii_conventions* section.

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,42 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import java.util.*
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        val thisDate = Calendar.getInstance()
+        val otherDate = Calendar.getInstance()
+
+        thisDate.set(year, month, dayOfMonth)
+        otherDate.set(other.year, other.month, other.dayOfMonth)
+
+        return ((thisDate.timeInMillis - otherDate.timeInMillis) / 86400000).toInt()
+    }
+
+}
+
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>, Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return DateIterator(this)
+    }
+
+    override fun contains(value: MyDate): Boolean {
+        return value >= start && value <= endInclusive
+    }
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+
+class DateIterator(private val dateRange: DateRange) : Iterator<MyDate> {
+    private var current : MyDate = dateRange.start
+    override fun hasNext(): Boolean = current <= dateRange.endInclusive
+
+    override fun next(): MyDate {
+        val temp = current
+        current = current.nextDay()
+        return temp
+    }
+}
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +44,9 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class MultipliedInterval(val timeInterval: TimeInterval, val repeatCount: Int)
+
+operator fun TimeInterval.times(number: Int) = MultipliedInterval(this, number)
+
+operator fun MyDate.plus(timeInterval: TimeInterval) = addTimeIntervals(timeInterval, 1)
+operator fun MyDate.plus(multipliedInterval: MultipliedInterval) = addTimeIntervals(multipliedInterval.timeInterval, multipliedInterval.repeatCount)
--- src/iii_conventions/n29OperatorsOverloading.kt
+++ src/iii_conventions/n29OperatorsOverloading.kt
@@ -19,13 +19,8 @@ fun todoTask29(): Nothing = TODO(
         date.addTimeIntervals(timeInterval, 1)
     })
 
-fun task29_1(today: MyDate): MyDate {
-    todoTask29()
-//    return today + YEAR + WEEK
-}
+fun task29_1(today: MyDate): MyDate = today + YEAR + WEEK
 
-fun task29_2(today: MyDate): MyDate {
-    todoTask29()
-//    return today + YEAR * 2 + WEEK * 3 + DAY * 5
-}
+fun task29_2(today: MyDate): MyDate = today + YEAR * 2 + WEEK * 3 + DAY * 5
+
 
